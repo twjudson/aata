c4f84e7778fc2d9f194290f049189607c86c7082
Rob Beezer <beezer@ups.edu>
2013-02-19 08:38:18 -0800

    Website updates, snapshot announcement

--- abstract.ups.edu/aata.js
+++ abstract.ups.edu/aata.js
@@ -5,7 +5,8 @@
 // <p><img alt="New" src="graphics/rednew.gif"></a> Follow the book on <a href="http://twitter.com/">Twitter</a> @ <a href="http://twitter.com/FCLAbook">FCLAbook</a>.</p>
 function news_flash(){
     // Include leading space after rednew graphic
-    flash = '<p><img alt="New" src="graphics/rednew.gif">  <a href="download.html">2012 Annual</a> edition now available (2012/08/11)</p>'
+    flash = '<p><img alt="New" src="graphics/rednew.gif">  <a href="download.html">Snapshot</a> edition now available (2013/02/19)</p>'
+    //flash = '<p><img alt="New" src="graphics/rednew.gif">  <a href="download.html">2012 Annual</a> edition now available (2012/08/11)</p>'
     // Comment/uncomment next line to turn off/on
     document.write(flash)
 }
