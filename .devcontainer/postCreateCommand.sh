f82615280efb036c96ba0c7d63c08ccd1844f3b2
Josh Harvey <124095653+jbrock1999h@users.noreply.github.com>
2023-04-20 01:32:27 +0000

    Modify: wording

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -9,7 +9,7 @@ pretext generate
 
 # Add new remote repositories for origin (forked) and upstream (original)
 git remote add upstream https://github.com/twjudson/aata
-# git remote add origin **How do you tell the url of the personal, forked branch when anyone could do so?
+# git remote add origin **How do you tell the url of the personal, forked branch when anyone could create one?
 
 # git pull origin main
 # git reset --hard origin/main
